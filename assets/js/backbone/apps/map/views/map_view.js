ff1c3a25e3c16298b9e79799cf81c29356b3420f
Sef Kloninger <sef@kloninger.com>
2015-04-14 23:37:23 -0700

    map: cleaner appearance, responsive, border

--- assets/js/backbone/apps/map/views/map_view.js
+++ assets/js/backbone/apps/map/views/map_view.js
@@ -4,6 +4,7 @@ var d3 = require('d3');
 var topojson = require('topojson');
 
 // code for world map adapted from https://gist.github.com/mbostock/4180634
+// and http://techslides.com/demos/d3/worldmap-template.html
 
 var MapView = Backbone.View.extend({
 
@@ -13,7 +14,24 @@ var MapView = Backbone.View.extend({
 
   initialize: function (options) {
     var that = this;
-    that.options = options;
+    this.options = options;
+    $(window).resize(_.debounce(function () {
+      that.render();
+    }, 200));
+
+    // shades of green, from http://colorbrewer2.org/
+    // TODO: make map colors configurable
+    this.color = d3.scale.ordinal()
+      .domain(d3.range(0, 7))
+      .range([
+        'rgb(204,236,230)',
+        'rgb(153,216,201)',
+        'rgb(102,194,164)',
+        'rgb(65,174,118)',
+        'rgb(35,139,69)',
+        'rgb(0,88,36)'
+      ]);
+
     d3.json("data/world-50m.json", function (error, world) {
       that.countries = topojson.feature(world, world.objects.countries).features;
       that.neighbors = topojson.neighbors(world.objects.countries.geometries);
@@ -25,67 +43,62 @@ var MapView = Backbone.View.extend({
       that.render();
     });
 
-    $(window).resize(_.debounce(function () {
-      that.render();
-    }, 600));
   },
 
   render: function () {
     var that = this;
-    var width = parseInt(d3.select(this.el).style('width'), 10);
-    var height = Math.round(width / 2);
+    that.width = parseInt(d3.select(that.el).style('width'), 10);
+    that.height = Math.round(that.width / 2);
 
-    this.$el.find('svg').remove();
-    var svg = d3.select(this.el).append('svg')
+    this.$el.addClass("mapborder");
+
+    that.$el.find('svg').remove();
+    var svg = d3.select(that.el).append('svg')
       .attr("preserveAspectRatio", "xMaxYMid")
       .attr("meetOrSlice", "slice")
-      .attr("viewBox", "0 0 " + width + " " + height)
-      .attr("width", width)
-      .attr("height", height);
+      .attr("viewBox", "0 0 " + that.width + " " + that.height)
+      .attr("width", that.width)
+      .attr("height", that.height);
 
-    var projection = d3.geo.mercator()
-      .scale(200)
-      .translate([width / 2, height / 3]);
+    that.projection = d3.geo.mercator()
+      .scale(that.width / 2 / Math.PI)
+      .translate([that.width / 2, that.height / 2]);
 
     var path = d3.geo.path()
-      .projection(projection);
-
-    var color = d3.scale.ordinal()
-      .domain(d3.range(0, 7))
-      .range([
-        'rgb(237,248,251)',
-        'rgb(204,236,230)',
-        'rgb(153,216,201)',
-        'rgb(102,194,164)',
-        'rgb(65,174,118)',
-        'rgb(35,139,69)',
-        'rgb(0,88,36)'
-      ]);
+      .projection(that.projection);
 
     svg.append("defs").append("path")
       .datum({type: "Sphere"})
       .attr("id", "sphere")
       .attr("d", path);
 
-    svg.append("use")
-      .attr("class", "stroke")
-      .attr("xlink:href", "#sphere");
-
     svg.selectAll(".country")
-      .each(function(n) {
+      .each(function (n) {
         delete that.countries[n].color;
       });
-
     svg.selectAll(".country")
       .data(that.countries)
-      .enter().insert("path", ".graticule")
+      .enter().insert("path", ".boundary")
       .attr("class", "country")
       .attr("d", path)
       .style("fill", function (d) {
-        return color(d.color)
+        return that.color(d.color);
       });
 
-    d3.select(self.frameElement).style("height", height + "px");
+    d3.select(self.frameElement).style("height", that.height + "px");
+  },
+
+  addpoint: function (lat, lon) {
+
+    var gpoint = g.append("g").attr("class", "gpoint");
+    var x = this.projection([lat, lon])[0];
+    var y = this.projection([lat, lon])[1];
+
+    gpoint.append("svg:circle")
+      .attr("cx", x)
+      .attr("cy", y)
+      .attr("class", "point")
+      .attr("r", 1.5);
   },
 
   cleanup: function () {
