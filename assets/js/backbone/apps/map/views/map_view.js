40861965217481a5512eb193b501b3e50d2935ef
Sef Kloninger <sef@kloninger.com>
2015-04-21 10:25:46 -0700

    improve map data flow and visuals
    Major improvements to the map
    
    - Data moved out of thew view into the controller; now map and user data
      fetched in parallel
    
    - Removed complicated refreshing logic and now just let SVG take care of
      scaling to make responsive. Cleaner and much faster on resize.
    
    - Styling moved into CSS
    
    - Cleaner visuals: white background, no coloring, simple blue for now
      (still hardcoded)

--- assets/js/backbone/apps/map/views/map_view.js
+++ assets/js/backbone/apps/map/views/map_view.js
@@ -1,110 +1,75 @@
-var _ = require('underscore');
-var Backbone = require('backbone');
-var d3 = require('d3');
-var topojson = require('topojson');
-
+// Use D3 to generate a nice looking world map with dots for our users.
+//
+// Note on picking the width and height in render(). This sets the
+// height and width based on how the view is originally scaled.
+// SVG scaling will take care of the rest to make it responsive.
+// This approach will be good for small screens, but can look bad
+// when you start small and scale up large.
+//
 // code for world map adapted from https://gist.github.com/mbostock/4180634
 // and http://techslides.com/demos/d3/worldmap-template.html
 
+var _ = require('underscore');
+var Backbone = require('backbone');
+var d3 = require('d3');
+
+
 var MapView = Backbone.View.extend({
 
-  events: {},
+    initialize: function (options) {
+      this.el = options.el;
+      this.people = options.people;
+      this.countries = options.countries;
+    },
 
-  initialize: function (options) {
-    var that = this;
-    this.el = options.el;
-    this.people = options.people;
+    render: function () {
+      this.width = this.$el.width();
+      this.height = Math.round(this.width / 2);
 
-    $(window).resize(_.debounce(function () {
-      that.render();
-    }, 200));
+      // Plot map
+      this.svg = d3.select(this.el).append('svg')
+        .attr("class", "userMap")
+        .attr("preserveAspectRatio", "xMaxYMid")
+        .attr("meetOrSlice", "slice")
+        .attr("viewBox", "0 0 " + this.width + " " + this.height);
 
-    // shades of green, from http://colorbrewer2.org/
-    // TODO: make map colors configurable
-    this.color = d3.scale.ordinal()
-      .domain(d3.range(0, 7))
-      .range([
-        'rgb(204,236,230)',
-        'rgb(153,216,201)',
-        'rgb(102,194,164)',
-        'rgb(65,174,118)',
-        'rgb(35,139,69)',
-        'rgb(0,88,36)'
-      ]);
+      this.projection = d3.geo.mercator()
+        .scale(this.width / 2 / Math.PI)
+        .translate([this.width / 2, this.height / 2]);
 
-    d3.json("data/world-50m.json", function (error, world) {
-      that.countries = topojson.feature(world, world.objects.countries).features;
-      that.neighbors = topojson.neighbors(world.objects.countries.geometries);
-      that.countries.forEach(function (c, i) {
-        c.color = d3.max(that.neighbors[i], function (n) {
-          return that.countries[n].color;
-        }) + 1 | 0
-      });
-      that.render();
-    });
+      var path = d3.geo.path()
+        .projection(this.projection);
 
-  },
+      this.svg.selectAll(".country")
+        .data(this.countries)
+        .enter().insert("path", ".boundary")
+        .attr("class", "country")
+        .attr("d", path);
 
-  render: function () {
-    var that = this;
-    that.width = parseInt(d3.select(that.el).style('width'), 10);
-    that.height = Math.round(that.width / 2);
+      // Plot users
+      var users = this.svg.append("g");
+      var locationTags = this.people
+        .pluck('location')
+        .filter(function (p) {
+          return !_.isUndefined(p)
+        });
 
-    that.$el.find('svg').remove();
-    that.svg = d3.select(that.el).append('svg')
-      .attr("preserveAspectRatio", "xMaxYMid")
-      .attr("meetOrSlice", "slice")
-      .attr("viewBox", "0 0 " + that.width + " " + that.height)
-      .attr("width", that.width)
-      .attr("height", that.height)
-      .attr("style", "border:solid 2px #222; border-radius: 5px");
+      _.each(locationTags, function (locationTag) {
+        var loc = locationTag.tag.data;
+        var userDot = users.append("g").attr("class", "userDot");
+        var projectedPoint = this.projection([loc.lon, loc.lat]);
+        userDot.append("svg:circle")
+          .attr("class", "point")
+          .attr("cx", projectedPoint[0])
+          .attr("cy", projectedPoint[1])
+          .attr("r", 6)
+      }, this);
+    },
 
-    that.projection = d3.geo.mercator()
-      .scale(that.width / 2 / Math.PI)
-      .translate([that.width / 2, that.height / 2]);
+    close: function () {
+      this.remove();
+    }
 
-    var path = d3.geo.path()
-      .projection(that.projection);
-
-    that.svg.selectAll(".country")
-      .data(that.countries)
-      .enter().insert("path", ".boundary")
-      .attr("class", "country")
-      .attr("d", path)
-      .style("fill", function (d) {
-        return that.color(d.color);
-      });
-
-    that.cities = that.svg.append("g");
-    this.people.on("sync", this.points, this);
-    this.people.fetch();
-  },
-
-  points: function (people) {
-    var that = this;
-    var locationTags = people
-      .pluck('location')
-      .filter(function (p) {return !_.isUndefined(p)});
-
-    locationTags.forEach(function (locationTag) {
-      var loc = locationTag.tag.data;
-      var gpoint = that.cities.append("g").attr("class", "gpoint");
-      var x = that.projection([loc.lon, loc.lat])[0];
-      var y = that.projection([loc.lon, loc.lat])[1];
-
-      gpoint.append("svg:circle")
-        .attr("cx", x)
-        .attr("cy", y)
-        .attr("class", "point")
-        .attr("r", 6)
-    });
-  },
-
-  cleanup: function () {
-    delete this.$el.find('svg');
-    removeView(this);
-  }
-
-});
+  });
 
 module.exports = MapView;
