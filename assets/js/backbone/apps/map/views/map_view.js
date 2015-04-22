d7bcb23923720239c6affd2429256cc74dbc390c
Sef Kloninger <sef@kloninger.com>
2015-04-21 22:40:19 -0700

    let svg handle responsive, data in controller

--- assets/js/backbone/apps/map/views/map_view.js
+++ assets/js/backbone/apps/map/views/map_view.js
@@ -1,88 +1,75 @@
-var _ = require('underscore');
-var Backbone = require('backbone');
-var d3 = require('d3');
-var topojson = require('topojson');
-
+// Use D3 to generate a nice looking world map with dots for our users.
+//
+// Note on picking the width and height in render(). This sets the
+// height and width based on how the view is originally scaled.
+// SVG scaling will take care of the rest to make it responsive.
+// This approach will be good for small screens, but can look bad
+// when you start small and scale up large.
+//
 // code for world map adapted from https://gist.github.com/mbostock/4180634
 // and http://techslides.com/demos/d3/worldmap-template.html
 
+var _ = require('underscore');
+var Backbone = require('backbone');
+var d3 = require('d3');
+
+
 var MapView = Backbone.View.extend({
 
-  events: {},
+    initialize: function (options) {
+      this.el = options.el;
+      this.people = options.people;
+      this.countries = options.countries;
+    },
 
-  initialize: function (options) {
-    var that = this;
-    this.el = options.el;
-    this.people = options.people;
+    render: function () {
+      this.width = this.$el.width();
+      this.height = Math.round(this.width / 2);
 
-    $(window).resize(_.debounce(function () {
-      that.render();
-    }, 200));
+      // Plot map
+      this.svg = d3.select(this.el).append('svg')
+        .attr("class", "userMap")
+        .attr("preserveAspectRatio", "xMaxYMid")
+        .attr("meetOrSlice", "slice")
+        .attr("viewBox", "0 0 " + this.width + " " + this.height);
 
-    d3.json("data/world-50m.json", function (error, world) {
-      that.countries = topojson.feature(world, world.objects.countries).features;
-      that.neighbors = topojson.neighbors(world.objects.countries.geometries);
-      that.render();
-    });
-  },
+      this.projection = d3.geo.mercator()
+        .scale(this.width / 2 / Math.PI)
+        .translate([this.width / 2, this.height / 2]);
 
-  render: function () {
-    var that = this;
-    that.width = parseInt(d3.select(that.el).style('width'), 10);
-    that.height = Math.round(that.width / 2);
+      var path = d3.geo.path()
+        .projection(this.projection);
 
-    that.$el.find('.userMap').remove();
-    that.svg = d3.select(that.el).append('svg')
-      .attr("class", "userMap")
-      .attr("preserveAspectRatio", "xMaxYMid")
-      .attr("meetOrSlice", "slice")
-      .attr("viewBox", "0 0 " + that.width + " " + that.height)
-      .attr("width", that.width)
-      .attr("height", that.height);
+      this.svg.selectAll(".country")
+        .data(this.countries)
+        .enter().insert("path", ".boundary")
+        .attr("class", "country")
+        .attr("d", path);
 
-    that.projection = d3.geo.mercator()
-      .scale(that.width / 2 / Math.PI)
-      .translate([that.width / 2, that.height / 2]);
+      // Plot users
+      var users = this.svg.append("g");
+      var locationTags = this.people
+        .pluck('location')
+        .filter(function (p) {
+          return !_.isUndefined(p)
+        });
 
-    var path = d3.geo.path()
-      .projection(that.projection);
+      _.each(locationTags, function (locationTag) {
+        var loc = locationTag.tag.data;
+        var userDot = users.append("g").attr("class", "userDot");
+        var projectedPoint = this.projection([loc.lon, loc.lat]);
+        userDot.append("svg:circle")
+          .attr("class", "point")
+          .attr("cx", projectedPoint[0])
+          .attr("cy", projectedPoint[1])
+          .attr("r", 6)
+      }, this);
+    },
 
-    that.svg.selectAll(".country")
-      .data(that.countries)
-      .enter().insert("path", ".boundary")
-      .attr("class", "country")
-      .attr("d", path);
+    close: function () {
+      this.remove();
+    }
 
-    that.cities = that.svg.append("g");
-    this.people.on("sync", this.points, this);
-    this.people.fetch();
-  },
-
-  points: function (people) {
-    var that = this;
-    var locationTags = people
-      .pluck('location')
-      .filter(function (p) {return !_.isUndefined(p)});
-
-    locationTags.forEach(function (locationTag) {
-      var loc = locationTag.tag.data;
-      var gpoint = that.cities.append("g").attr("class", "gpoint");
-      var x = that.projection([loc.lon, loc.lat])[0];
-      var y = that.projection([loc.lon, loc.lat])[1];
-
-      gpoint.append("svg:circle")
-        .attr("class", "point")
-        .attr("cx", x)
-        .attr("cy", y)
-        .attr("r", 6)
-    });
-  },
-
-  cleanup: function () {
-    delete this.$el.find('svg');
-    removeView(this);
-  }
-
-});
+  });
 
 module.exports = MapView;
